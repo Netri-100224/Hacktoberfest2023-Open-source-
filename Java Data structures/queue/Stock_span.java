83297d2633f2c43caab58527fc3cda4b88fe8e99
Netri-100224 <115025996+Netri-100224@users.noreply.github.com>
2023-10-23 22:44:01 +0530

    Create Stock_span.java

---
+++ Java Data structures/queue/Stock_span.java
@@ -0,0 +1,45 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+ 
+class Span {
+ 
+    static ArrayList<Integer> calculateSpan(int arr[],
+                                            int n)
+    {
+        // Your code here
+        Deque<Integer> s = new ArrayDeque<Integer>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i])
+                s.pop();
+ 
+            if (s.isEmpty())
+                ans.add(i + 1);
+            else {
+                int top = s.peek();
+                ans.add(i - top);
+            }
+            s.push(i);
+        }
+ 
+        return ans;
+    }
+ 
+    // A utility function to print elements of array
+    static void printArray(ArrayList<Integer> arr)
+    {
+        for (int i = 0; i < arr.size(); i++)
+            System.out.print(arr.get(i) + " ");
+    }
+ 
+    // Driver code
+    public static void main(String args[])
+    {
+        int price[] = { 10, 4, 5, 90, 120, 80 };
+        int n = price.length;
+ 
+        ArrayList<Integer> arr = calculateSpan(price, n);
+        printArray(arr);
+    }
+}
